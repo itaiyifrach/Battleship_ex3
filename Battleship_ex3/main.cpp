9c7d16c6a63c71ad2bedea2add69f05bcd3044e2
itaiyifrach <itai.yifrach@gmail.com>
2017-06-12 20:25:47 +0300

    edit

--- Battleship_ex3/main.cpp
+++ Battleship_ex3/main.cpp
@@ -1,18 +1,20 @@
 #include "GameUtils.h"
 #include "BattleshipGame.h"
 
+#define THREADS_DEFAULT 4
+
 int main(int argc, char** argv) 
 {
 	string basePath;
 	vector<string> boardNames;
 	vector<tuple<char3DArray,int,int,int,int>> boardVec;
 	vector<pair<unique_ptr<IBattleshipGameAlgo>, HINSTANCE>> playersVec;
-	int numOfBoards = 0, numOfPlayers = 0, numOfGames = 0, numOfThreads = 4;
+	int numOfBoards = 0, numOfPlayers = 0, numOfGames = 0, numOfThreads = THREADS_DEFAULT;
 	
 	GameUtils::parseArgs(argc, argv, basePath, numOfThreads);
 	auto check = GameUtils::parsePath(basePath, boardNames);
 	//check if path was invalid(error printed inside function)
-	if(check == -2)
+	if (check == -2)
 	{
 		return -1;
 	}
@@ -20,13 +22,13 @@ int main(int argc, char** argv)
 	//creates a vector of all *.dll files in the given path
 	vector<string> dllNames = GameUtils::getDLLNames(basePath);
 	//check if number of dll's/boards is invalid (errors printed inside functions)
-	if(dllNames.size() < 2 || check == -1)
+	if (dllNames.size() < 2 || check == -1)
 	{
 		return -1;
 	}
 	//loads all boards
 	numOfBoards = GameUtils::getBoards(basePath, boardNames, boardVec);
-	cout << "# of Valid BOARDS = " << numOfBoards << endl;
+	//cout << "# of Valid BOARDS = " << numOfBoards << endl;
 	
 	//loads all dll's
 	numOfPlayers = GameUtils::getPlayers(basePath,dllNames,playersVec);
@@ -34,7 +36,7 @@ int main(int argc, char** argv)
 	cout << LEGAL_PLAYERS << numOfPlayers << endl;
 	cout << LEGAL_BOARDS << numOfBoards << endl;
 
-	if(numOfBoards < 1 || numOfPlayers < 2)
+	if (numOfBoards < 1 || numOfPlayers < 2)
 	{
 		cout << NOT_ENOUGH_LEGAL << endl;
 		GameUtils::freeLibs(playersVec);
