3c65b03c2083c59746bafc9956c79e5a1d113886
itaiyifrach <itai.yifrach@gmail.com>
2017-06-14 17:42:30 +0300

    itai fs

---
+++ Battleship_ex3/CompetitionManager.cpp
@@ -0,0 +1,30 @@
+#include "CompetitionManager.h"
+
+void CompetitionManager::printResults(int fixture) {
+	// creating vector of 
+	vector<playerData> results;
+	for (int i = 0; playersData.size(); i++) {
+		results.push_back(playersData[1][fixture - 1]);
+	}
+
+	// sorting by wins percentage
+	sort(results.begin(), results.end(), percentCompare);
+
+	//------------title------------//
+	cout << setw(8) << left << "#" << setw(24) << left << "Team Name" << setw(8) << left << "Wins";
+	cout << setw(8) << left << "Losses" << setw(8) << left << "%" << setw(8) << left << "Pts For" << setw(12) << left << "Pts Against";
+	cout << endl << endl;
+	//-----------------------------//
+	for (size_t i = 0; i<results.size(); i++) {
+		string num = std::to_string(i + 1);
+		num = num + '.';
+		cout << setw(8) << left << num << setw(24) << left << names[i] << setw(8) << left << wins[i];
+		cout << setw(8) << left << losses[i] << setw(8) << left << percent[i] << setw(8) << left << pts_for[i] << setw(12) << left << pts_against[i];
+		cout << endl;
+	}
+
+}
+
+bool percentCompare(playerData p1, playerData p2) { 
+	return ((p1.wins / p1.gamesPlayed) > (p2.wins / p2.gamesPlayed));
+}
